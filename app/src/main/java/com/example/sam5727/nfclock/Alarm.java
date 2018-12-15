1e9348e9c517b21d358f6a9be72b36fbbbfad119
sam5727 <x6513750@gmail.com>
2018-12-15 14:03:22 +0800

    Choose the device alarm as volume

--- app/src/main/java/com/example/sam5727/nfclock/Alarm.java
+++ app/src/main/java/com/example/sam5727/nfclock/Alarm.java
@@ -1,23 +1,43 @@
 package com.example.sam5727.nfclock;
 
+import android.app.AlarmManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.PowerManager;
 import android.os.Vibrator;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 public class Alarm extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "ALARM", Toast.LENGTH_LONG).show();
+
         Vibrator v = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
         v.vibrate(5000);
-        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        MediaPlayer player = MediaPlayer.create(context, notification);
-        player.start();
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);;
+        int actualVolume = audioManager.getStreamVolume(AudioManager.STREAM_ALARM);
+        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, actualVolume, AudioManager.ADJUST_SAME);
+
+        MediaPlayer player = new MediaPlayer();
+        try {
+            player.setAudioStreamType(AudioManager.STREAM_ALARM);
+            player.setDataSource(context, notification);
+            player.setLooping(true);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/sam5727/nfclock/ClockAdapter.java
+++ app/src/main/java/com/example/sam5727/nfclock/ClockAdapter.java
@@ -5,6 +5,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
